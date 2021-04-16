a1025979bd350ddfd4c22de37403e6eeac63f6c5
fenghuihang <909206742@qq.com>
2021-04-16 10:17:16 +0800

    create-react-app完成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Layout, Menu } from 'antd';
+
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1">主页</Menu.Item>
+        <Menu.Item key="2">发起投票</Menu.Item>
+        <Menu.Item key="3">查看投票</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <div className="site-layout-content">Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>4Meeting ©2021 Created by 20174493@stu.neu.edu.cn</Footer>
+  </Layout>
   );
 }
 
